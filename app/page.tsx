43270f3cc3f591b49481a537a1ebee69ad0b8f4d
gokulramms <gokulramms@gmail.com>
2025-03-19 11:44:57 +0530

    bolbity fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import Hero from "./hero-section/Hero";
-import useBlobity from "blobity/lib/react/useBlobity";
-import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import { useEffect, useRef, useState } from "react";
 import { ScrollerMotion } from "scroller-motion";
 import PreLoader from "./animations/PreLoader/PreLoader";
 import { initialBlobityOptions } from "./utils/BlobityConfig";
 import NavBar from "./navbar/NavBar";
 
-import dynamic from "next/dynamic";
 const Work = dynamic(() => import("./work-section/Work"));
 const About = dynamic(() => import("./about-section/About"));
 const Blog = dynamic(() => import("./blog-section/BlogGrid"));
@@ -15,29 +14,24 @@ const Contact = dynamic(() => import("./contact-section/Contact"));
 const Footer = dynamic(() => import("./footer/Footer"));
 
 export default function Home() {
-  const blobityInstance = useBlobity(initialBlobityOptions);
+  const blobityInstance = useRef<any>(null); // Prevents re-renders
 
   useEffect(() => {
-    if (blobityInstance.current) {
-      // @ts-ignore for debugging purposes or playing around
-      window.blobity = blobityInstance.current;
-    }
-  }, [blobityInstance]);
+    // Dynamically import blobity on the client-side
+    import("blobity").then((Blobity) => {
+      blobityInstance.current = new Blobity.default(initialBlobityOptions);
+      window.blobity = blobityInstance.current; // For debugging
+    }).catch((err) => console.error("Failed to load Blobity:", err));
+  }, []); // Runs only once on mount
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-    });
+    window.scrollTo({ top: 0, left: 0 });
   }, []);
 
   return (
     <>
       <PreLoader />
-
       <NavBar />
-
-      {/* <ScrollerMotion> */}
       <main className="flex flex-col items-center justify-center">
         <Hero />
         <Work />
@@ -46,7 +40,6 @@ export default function Home() {
         <Contact />
         <Footer />
       </main>
-      {/* </ScrollerMotion> */}
     </>
   );
 }
